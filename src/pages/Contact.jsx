bb397d02b09db11f4ceedc12c354c2642ffa933c
guitarbuju <gerardoamadrid@gmail.com>
2024-09-23 14:39:46 +0200

    trying again

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -180,33 +180,3 @@ const Contact = () => {
 
 export default Contact;
 
-// const ContactTextArea = ({ row, placeholder, name, defaultValue }) => {
-//   return (
-//     <>
-//       <div className="mb-6">
-//         <textarea
-//           rows={row}
-//           placeholder={placeholder}
-//           name={name}
-//           className="border-[f0f0f0] w-full resize-none rounded border py-3 px-[14px] text-base text-body-color outline-none focus:border-primary focus-visible:shadow-none"
-//           defaultValue={defaultValue}
-//         />
-//       </div>
-//     </>
-//   );
-// };
-
-// const ContactInputBox = () => {
-//   return (
-//     <>
-//       <div className="mb-6">
-//         <input
-//           type={type}
-//           placeholder={placeholder}
-//           name={name}
-//           className="border-[f0f0f0] w-full rounded border py-3 px-[14px] text-base text-body-color outline-none focus:border-primary focus-visible:shadow-none"
-//         />
-//       </div>
-//     </>
-//   );
-// };
